e5407d72825ddb8c807e222f2bf49e81db2e91e5
enven-omiomi <enven.omiomi@gmail.com>
2024-01-22 11:49:25 +0900

    add output option

--- index.js
+++ index.js
@@ -10,7 +10,28 @@ const cssPath = path.resolve('node_modules/zenn-content-css/lib/index.css');
 const cssContent = fs.readFileSync(cssPath, 'utf-8');
 
 // コマンドライン引数からMarkdownファイルのパスを取得
-const markdownFiles = process.argv.slice(2);
+// const markdownFiles = process.argv.slice(2);
+
+// コマンドライン引数を解析する関数
+function parseArgs(args) {
+  const options = {
+    output: 'out/output.html', // デフォルトの出力先
+    markdownFiles: []
+  };
+
+  for (let i = 2; i < args.length; i++) {
+    if (args[i] === '-o' && args[i + 1]) {
+      options.output = args[i + 1];
+      i++; // オプションの値をスキップ
+    } else {
+      options.markdownFiles.push(args[i]);
+    }
+  }
+
+  return options;
+}
+
+const options = parseArgs(process.argv);
 
 // Markdownファイルを読み込んで1つの文字列に結合
 const readMarkdownFiles = (files) => {
@@ -20,10 +41,10 @@ const readMarkdownFiles = (files) => {
   }).join('\n\n');
 };
 
-const markdownContent = readMarkdownFiles(markdownFiles);
+const markdownContent = readMarkdownFiles(options.markdownFiles);
 
 const htmlContent = markdownToHtml(markdownContent);
 const wrappedHtmlContent = `<style>\n${cssContent}\n</style>\n<div class="znc">\n${htmlContent}\n</div>`;
 
 // HTMLファイルに出力（例：output.html）
-fs.writeFileSync("out/output.html", wrappedHtmlContent);
+fs.writeFileSync(options.output, wrappedHtmlContent);
